bfb12b4ba42b9844a8ac910d57bb3bfeb48b673a
Slava Khripushin <slava.cpa@yandex.ru>
2025-08-01 17:52:46 +0700

    feat: catalog

--- src/js/main.js
+++ src/js/main.js
@@ -47,4 +47,54 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+  document.querySelectorAll('.catalog-card').forEach((card) => {
+    const pic = card.querySelector('.catalog-card__pic');
+    const img = pic.querySelector('.catalog-card__image');
+    const images = pic.dataset.images.split(',');
+    const zoneCount = images.length;
+
+    let isHovering = false;
+
+    pic.addEventListener('mouseenter', () => {
+      isHovering = true;
+    });
+
+    pic.addEventListener('mousemove', (e) => {
+      if (!isHovering) return;
+
+      const rect = pic.getBoundingClientRect();
+      const relativeX = e.clientX - rect.left;
+      const zoneWidth = rect.width / zoneCount;
+      const index = Math.max(0, Math.min(zoneCount - 1, Math.floor(relativeX / zoneWidth)));
+
+      img.src = images[index];
+    });
+
+    pic.addEventListener('mouseleave', () => {
+      isHovering = false;
+      img.src = images[0];
+    });
+
+    const favoriteIcon = card.querySelector('.catalog-card__favorite');
+    favoriteIcon.addEventListener('click', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      favoriteIcon.classList.toggle('liked');
+    });
+  });
+
+  const searchBlock = document.querySelector('.catalog-filter__search');
+  const searchText = document.querySelector('.catalog-filter__search-text');
+
+  searchText?.addEventListener('click', function (e) {
+    searchBlock.classList.add('show');
+    e.stopPropagation();
+  });
+
+  document.addEventListener('click', function (e) {
+    if (!searchBlock.contains(e.target)) {
+      searchBlock.classList.remove('show');
+    }
+  });
 });

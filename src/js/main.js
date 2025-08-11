4b04b1676cf5c08bf11f85f5919ba1767aacded4
Slava Khripushin <slava.cpa@yandex.ru>
2025-08-11 15:18:29 +0700

    feat: collection page

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+  Fancybox.bind('[data-fancybox]', {
+    dragToClose: false,
+    autoFocus: false,
+    placeFocusBack: false,
+    Thumbs: false,
+    Images: {
+      zoom: false,
+    },
+  });
+
   const maskOptions = {
     mask: '+7 (000) 000-00-00',
     onFocus() {
